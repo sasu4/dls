1d6d1fd92ae85e33978b5e988ccd51a01f01f0be
Student <Student@KI_CQ615>
2015-06-09 12:25:00 +0200

    add form and dx_auth autoloading

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -60,7 +60,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
 
-$autoload['libraries'] = array('database');
+$autoload['libraries'] = array('database','DX_Auth');
 
 
 /*
@@ -88,7 +88,7 @@ $autoload['drivers'] = array();
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url','my');
+$autoload['helper'] = array('url','my','form');
 
 
 /*
