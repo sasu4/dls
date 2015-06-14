bc3f4a15b76b477b17b32d2281c2d3790026af46
Lucka <Lucka@Lucka-NB>
2015-06-14 20:50:10 +0200

    profil a studenti

--- application/views/profile/students.php
+++ application/views/profile/students.php
@@ -1,18 +1,66 @@
+<table cellspacing="50" cellpadding="10">
 <?php
-
 if($query->num_rows()>0) {
-    foreach($query->result() as $row) {
-        echo $row->bc;
-        echo $row->mgr;
-        echo $row->phd;
-        echo $row->nonskv;
-        echo $row->public;
-        echo $row->year;
-        //Aký je momentálny počet študentov podľa stupňa jazykovej kompetencie?
-        echo $row->a1;
-        echo $row->a2;
-        echo $row->b1;
-        echo $row->b2;
-        echo $row->c1;
+    foreach($query->result() as $row) {?>
+<tr>
+    <td><b>Bakalári</b></td>
+    <td><?php echo $row->bc ?></td>
+</tr>
+<tr>
+    <td><b>Magistri</b></td>
+    <td><?php echo $row->mgr ?></td>
+</tr>
+<tr>
+    <td><b>Doktorandi</b></td>
+    <td><?php echo $row->mgr ?></td>
+</tr>
+<tr>
+    <td><b>Študenti inej národnosti</b></td>
+    <td><?php echo $row->nonsvk ?></td>
+</tr>
+<tr>
+    <td><b>Počet študentov v kurzoch pre verejnosť</b></td>
+    <td><?php echo $row->public ?></td>
+</tr>
+<tr>
+    <td><b>Stav k roku</b></td>
+    <td><?php echo $row->year ?></td>
+</tr>
+<tr>
+    <td><b>&nbsp;</b></td>
+    <td>&nbsp;</td>
+</tr>
+<tr>
+    <td><b>&nbsp;</b></td>
+    <td>&nbsp;</td>
+</tr>
+<tr>
+    <td><b>&nbsp;</b></td>
+    <td>&nbsp;</td>
+</tr>
+<tr>
+    <td><b>A1</b></td>
+    <td><?php echo $row->a1 ?></td>
+</tr>
+<tr>
+    <td><b>A2</b></td>
+    <td><?php echo $row->a2 ?></td>
+</tr>
+<tr>
+    <td><b>B1</b></td>
+    <td><?php echo $row->b1 ?></td>
+</tr>
+<tr>
+    <td><b>B2</b></td>
+    <td><?php echo $row->b2 ?></td>
+</tr>
+<tr>
+    <td><b>C1</b></td>
+    <td><?php echo $row->c1 ?></td>
+</tr>
+</table>
+<?php
     }
 }
+?>
+
--- application/views/profile/workplace.php
+++ application/views/profile/workplace.php
@@ -1,18 +1,60 @@
+<table cellspacing="50" cellpadding="10">
 <?php
-
 if($query->num_rows()>0) {
-    foreach($query->result() as $row) {
-        echo $row->name_orig;
-        echo $row->name_sk;
-        echo $row->univ_orig;
-        echo $row->univ_sk;
-        echo $row->street;
-        echo $row->number;
-        echo $row->city;
-        echo $row->country_id;
-        echo $row->zip;
-        echo $row->telephone;
-        echo $row->website;
-        echo $row->focus;
+    foreach($query->result() as $row) {?>
+    <h1>
+<?php echo $row->name_orig ?>
+    </h1>
+<tr>
+    <td><b>Name</b></td>
+    <td><?php echo $row->name_orig ?></td>
+</tr>
+<tr>
+    <td><b>Názov(svk)</b></td>
+    <td><?php echo $row->name_sk ?></td>
+</tr>
+<tr>
+    <td><b>University</b></td>
+    <td><?php echo $row->univ_orig ?></td>
+</tr>
+<tr>
+    <td><b>Univerzita (svk)</b></td>
+    <td><?php echo $row->univ_sk ?></td>
+</tr>
+<tr>
+    <td><b>Ulica</b></td>
+    <td><?php echo $row->street ?></td>
+</tr>
+<tr>
+    <td><b>Číslo</b></td>
+    <td><?php echo $row->number ?></td>
+</tr>
+<tr>
+    <td><b>Mesto</b></td>
+    <td><?php echo $row->city ?></td>
+</tr>
+<tr>
+    <td><b>ID krajiny</b></td>
+    <td><?php echo $row->country_id ?></td>
+</tr>
+<tr>
+    <td><b>zip</b></td>
+    <td><?php echo $row->zip ?></td>
+</tr>
+<tr>
+    <td><b>Telefonický kontakt</b></td>
+    <td><?php echo $row->telephone ?></td>
+</tr>
+<tr>
+    <td><b>Webová stránka</b></td>
+    <td><?php echo $row->website ?></td>
+</tr>
+<tr>
+    <td><b>Zameranie</b></td>
+    <td><?php echo $row->focus ?></td>
+</tr>
+</table>
+<?php
     }
 }
+?>
