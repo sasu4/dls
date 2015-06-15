ffce4cf9bbe6ae04de8328eb9cfd9a7b643a1b87
lubo <lubo@lubos-pc>
2015-06-15 22:22:56 +0200

    logo a trochu vzhladu

--- application/views/home.php
+++ application/views/home.php
@@ -1,6 +1,6 @@
 <p>Welcome to The Slovakists Around the World Database.</p><br>
 
-<table align="center">
+<table>
 <?php
 $id = 0;
 if ($coun->num_rows() > 0) {
@@ -25,27 +25,6 @@ if ($coun->num_rows() > 0) {
     echo '</ul></td></tr>';
 }
 ?>
-  
-<!--    <tr><td><ul><li>Sofia</li></ul></td><td></td><td></td></tr>
-    <tr><td><ul><li>Veľké Tarnovo</li></ul></td><td></td><td></td></tr>
-    <tr><td><b>Čína</b></td><td></td><td></td></tr>
-    <tr><td><b>Chorvátsko</b></td><td></td></tr>
-    <tr><td><b>Fínsko</b></td><td> Rumunsko</td></tr>
-    <tr><td><b>Francúzsko</b></td></tr>
-    <tr><td><ul><li>Clermont - Ferrand</li></ul></td><td></td><td></td></tr>
-    <tr><td><ul><li>Paríž</li></ul></td><td></td><td></td></tr>
-    <tr><td><ul><li>Štrasburg</li></ul></td><td></td><td></td></tr>
-    <tr><td><b>Maďarsko</b></td></tr>
-    <tr><td> Nemecko</td></tr>
-    <tr><td> Poľsko</td></tr>
-    <tr><td> Rakúsko</td></tr>
-    <tr><td> Rumunsko</td></tr>
-    <tr><td> Rusko</td></tr>
-    <tr><td> Slovinsko</td></tr>
-    <tr><td> Srbsko</td></tr>
-    <tr><td> Taliansko</td></tr>
-    <tr><td> USA - Spojené štáty americké</td></tr>
-    <tr><td> Ukrajina</td></tr>-->
 </table>
 <?php
     echo anchor('home/pages/about','About');
@@ -61,22 +40,4 @@ if ($coun->num_rows() > 0) {
     echo anchor('home/pages/more','More activities');
 ?>
 <br>
-<!--<h2>Lectorates</h2>-->
-<?php
-//if ($list->num_rows() > 0) {
-//    foreach ($list->result() as $row) {
-//        echo anchor('lectorate/lect/'.$row->id,$row->name_orig);
-//        echo '<br>';
-//    }
-//}?>
-<!--<h2>Countries</h2>-->
-<?php
-//if ($coun->num_rows() > 0) {
-//    foreach ($coun->result() as $row) {
-//        $data = $this->lectorate->get_lectorate_c($row->country_id);
-//        echo anchor('country/'.$data['id'],$data['name']);
-//        echo '<br>';
-//    }
-//}
-
     
