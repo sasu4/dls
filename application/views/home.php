80070a84caa2e68d9843e350f8f2d10188359258
lubo <lubo@lubos-pc>
2015-06-16 21:40:25 +0200

    zatial netreba stranky

--- application/views/home.php
+++ application/views/home.php
@@ -27,17 +27,17 @@ if ($coun->num_rows() > 0) {
 ?>
 </table>
 <?php
-    echo anchor('home/pages/about','About');
-    echo '<br>';
-    echo anchor('home/pages/services','Services');
-    echo '<br>';
-    echo anchor('home/pages/people','People');
-    echo '<br>';
-    echo anchor('home/pages/contact','Contact');
-    echo '<br>';
-    echo anchor('home/pages/sas','SAS');
-    echo '<br>';
-    echo anchor('home/pages/more','More activities');
+//    echo anchor('home/pages/about','About');
+//    echo '<br>';
+//    echo anchor('home/pages/services','Services');
+//    echo '<br>';
+//    echo anchor('home/pages/people','People');
+//    echo '<br>';
+//    echo anchor('home/pages/contact','Contact');
+//    echo '<br>';
+//    echo anchor('home/pages/sas','SAS');
+//    echo '<br>';
+//    echo anchor('home/pages/more','More activities');
 ?>
 <br>
     
