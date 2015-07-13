26b7c80c431b818588b9f7654f5ed045bc770145
lubo <lubo@lubos-pc>
2015-07-13 22:52:13 +0200

    - upravit pekne new_lectorate

--- application/views/new_lectorate.php
+++ application/views/new_lectorate.php
@@ -1,31 +1,40 @@
-<h2>Pridajte nový lektorát</h2>
-<p>Aký je presný názov pracoviska, pod ktoré spadá lektorát slovenského jazyka a kultúry, respektíve kde sa vyučuje slovenský jazyk a kultúra? (uveďte originálny aj preložený názov).</p>
-<table>
-    <?php echo form_open('lectorate/add_lect'); ?>
-    <tr>
-        <td><?php echo form_label('Originálny názov');?></td>
-        <td><?php echo form_input('lect_name_o','');?></td>
-    </tr>
-    <tr>
-        <td><?php echo form_label('Preložený názov');?></td>
-        <td><?php echo form_input('lect_name_s','');?></td>
-    </tr>
-    <tr>
-        <td><?php echo form_label('Krajina');?></td>
-        <td><?php echo form_dropdown('country_id',$countries);?></td>
-    </tr>
-    <tr>
-        <td><?php echo form_label('Typ organizácie');?></td>
-        <td><?php $type = array(
-                'lektorát' => 'lektorát',
-                'iné' => 'iné'
-            );
-        echo form_dropdown('organization',$type);?></td>
-    </tr>
-    <tr>
-        <td><?php echo form_submit('lectorate/add_lect','Vytvoriť'); ?></td>
-    </tr>
-<?php
-    echo form_close();
-?>
-</table>
+<div class="container">
+    <div class="row"><br /></div>
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Pridajte nový lektorát</h3>
+                </div>
+                <div class="panel-body">
+                    <?php echo form_open('lectorate/add_lect'); ?>
+                    <div class="form-group">
+                        <?php echo form_label('Aký je presný názov pracoviska, pod ktoré spadá lektorát slovenského jazyka a kultúry, respektíve kde sa vyučuje slovenský jazyk a kultúra? (uveďte originálny aj preložený názov).');?>
+                        <?php echo form_label('Originálny názov');?>
+                        <?php echo form_input('lect_name_o','','class="form-control"');?><br />
+                        <?php echo form_label('Preložený názov');?>
+                        <?php echo form_input('lect_name_s','','class="form-control"');?>
+                    </div>
+                    <div class="form-group">
+                        <?php echo form_label('Krajina');?>
+                        <?php echo form_dropdown('country_id',$countries,'Slovakia','class="form-control"');?>
+                    </div>
+                    <div class="form-group">
+                        <?php echo form_label('Typ organizácie');?><br>
+                        <?php $type = array(
+                            'lektorát' => 'lektorát',
+                            'iné' => 'iné'
+                        );
+                        echo form_dropdown('organization',$type,'lektorát','class="form-control"');?>
+                    </div>
+                    <hr />
+                        <?php echo form_submit('lectorate/add_lect','Vytvoriť','class="btn btn-lg btn-success btn-block"'); ?>
+                        <?php echo anchor('home', 'Späť', 'class="btn btn-lg btn-info btn-block"'); ?>
+                    <?php
+                        echo form_close();
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /.container je ukonceny vo footer -->
