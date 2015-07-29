aff9b3065fb4c2c97a5a5ce9bc2ae18625fc83fa
Lubo <lubomir.benko@gmail.com>
2015-07-29 13:07:55 +0200

    vymazanie my_helper

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -88,7 +88,7 @@ $autoload['drivers'] = array();
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url','my','form');
+$autoload['helper'] = array('url','form');
 
 
 /*
