03c931cdd92799209fb894e03fef4ae098edddaa
Jakub <Jakub@10.10.106.12>
2016-02-02 14:22:32 +0100

    vzhlad

--- application/views/Auth/general_message.php
+++ application/views/Auth/general_message.php
@@ -1,1 +1,7 @@
-<?php echo $auth_message ?>
+<section id="message" class="container">
+<div class="container">
+    <div class="row">
+        <?php echo $auth_message ?>
+    </div>
+</div>
+</section>
