dc63cce8aff030ff517cae9ec34be7d484cfac21
Jakub <Jakub@10.10.106.12>
2016-02-03 10:33:50 +0100

    kontaktny formular

--- application/controllers/Contact.php
+++ application/controllers/Contact.php
@@ -22,9 +22,30 @@ class Contact extends CI_Controller {
     public function index() {
         $this->load->view('header2');
         $this->load->view('navigation2');
-        //  $this->load->view('welcome_message');
         $this->load->view('contact');
         $this->load->view('footer2');
     }
+    
+    public function sent() {
+        $this->load->library('email');
+        $mail = $this->input->post('email');
+        $name = $this->input->post('name');
+        $surname = $this->input->post('surname');
+        $subject = $this->input->post('subject');
+        $message = $this->input->post('message');
+        $this->email->from($mail,$name.' '.$surname);
+        $this->email->to($this->config->item('DX_webmaster_email'));
+        
+        $this->email->subject('Kontaktný formulár - '.$subject);
+        $this->email->message($message);
+        
+        $this->email->send();
+        
+        $this->load->view('header2');
+        $this->load->view('navigation2');
+        $this->load->view('contact_sent');
+        $this->load->view('footer2');
+    }
+    
 
 }
