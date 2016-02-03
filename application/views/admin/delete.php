0e7c5aafbfaf186aeb0ba01aa3cb003ceaaab105
lubo <lubo@lubos-pc>
2016-02-03 19:01:20 +0100

    mazanie noviniek

---
+++ application/views/admin/delete.php
@@ -0,0 +1,25 @@
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <div class="login-panel panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Naozaj si želáte odstrániť vybranú položku?</h3>
+                </div>
+                <div class="panel-body">
+                    <div class="col-md-6"><div class="left">
+                <?php 
+                    echo anchor('admin/delete_news/'.$id,'Odstrániť','class="btn btn-lg btn-danger"');
+                ?>
+                    </div></div>
+                    <div class="col-md-6"> <div class="right">
+                <?php
+                    echo anchor('admin/news','Zrušiť','class="btn btn-lg btn-primary"');
+                ?>
+                    </div></div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
