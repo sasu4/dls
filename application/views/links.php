7121589cb1595f55206518ba24b1e5d65d264418
Jakub <Jakub@10.10.106.12>
2016-02-03 14:15:36 +0100

    zmeny martin

--- application/views/links.php
+++ application/views/links.php
@@ -2,30 +2,44 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
-                <div class="center">
+                <div>
                     <h2>Zaujímavé odkazy</h2>
-                    <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+                    <p></p>
                 </div>
                 <div class="gap"></div>
                 <div class="row">
                     <div class="col-md-6">
                         <blockquote>
-                            <p><a href="http://fphil.uniba.sk/sas/" target="_blank">Studia Academica Slovaca</a></p>
-                            <small>Someone famous in <cite title="Source Title">Studia Academica Slovaca</cite></small>
+                            <p><a href="http://fphil.uniba.sk/sas/" target="_blank">Studia Academica Slovaca</a><p>
+                            <p><small>Studia Academica Slovaca – centrum pre slovenčinu ako cudzí jazyk (SAS) je špecializovaným pracoviskom Filozofickej fakulty Univerzity Komenského (FF UK) v Bratislave. Ťažiskom pedagogickej a vedeckovýskumnej činnosti je vzdelávanie zahraničných záujemcov o slovenský jazyk a kultúru, propagácia slovenskej vedy, kultúry a umenia v zahraničí, realizácia a koordinácia výskumu slovenčiny ako cudzieho jazyka, riešenie medzinárodných a domácich vedeckovýskumných projektov a edičná činnosť zameraná na tvorbu a vydávanie vedeckých slovakistických publikácií a učebníc slovenčiny ako cudzieho jazyka.</small></p>
                         </blockquote>
                         <blockquote>
                             <p><a href="http://myslovak.sk/cms/" target="_blank">My Slovak CMS</a></p>
-                            <small>Someone famous in <cite title="Source Title">My Slovak</cite></small>
+                            <p><small>MySlovak je e-learningový kurz slovenčiny ako cudzieho jazyka pre úplných začiatočníkov, ktorý Vám umožní dosiahnuť komunikatívnu kompetenciu v najbežnejších komunikačných situáciách. Obsahuje dialógy, ilustrácie, audionahrávky a cvičenia a vďaka svojej online podobe Vám poskytuje možnosť bezprostrednej spätnej väzby pri priebežnom testovaní dosiahnutých znalostí.</small></p>
+                        </blockquote>
+                        <blockquote>
+                            <p><a href="http://jazykovaporadna.sme.sk/" target="_blank">Jazyková poradňa</a></p>
+                            <p>
+                                <small>Jazyková poradňa je spoločným projektom portálu Sme.sk a Jazykovedného ústavu Ľudovíta Štúra SAV. Sme.sk zabezpečuje technickú podporu a pracovníci JÚĽŠ SAV zabezpečujú odborné jazykové poradenstvo.
+                                </small>
+                            </p>
                         </blockquote>
                     </div>
                     <div class="col-md-6">
                         <blockquote>
-                            <p></p>
-                            <small>Someone famous in <cite title="Source Title">Source Title</cite></small>
+                            <p><a href="http://e-slovak.sk/" target="_blank">Dištančná forma štúdia slovenčiny</a></p>
+                            <p><small>E-slovak is an e-learning course of Slovak as a foreign language aimed at acquiring basic language skills which can be applied to day-to-day situations. The course corresponds with the textbook Krížom-krážom. Slovenčina A1 – one of the series of textbooks of Slovak which were published by Studia Academica Slovaca – The Centre for Slovak as a Foreign Language.</small>
+                            </p>
                         </blockquote>
                         <blockquote>
-                            <p>.</p>
-                            <small>Someone famous in <cite title="Source Title">Source Title</cite></small>
+                            <p><a href="http://zborniky.e-slovak.sk/" target="_blank">Zborníky</a></p>
+                            <p><small>Zborníky prednášok Letnej školy slovenského jazyka a kultúry Studia Academica Slovaca sú Vám sprístupnené v elektronickej forme.
+                                    Projekt elektronizácie zborníkov prednášok Letnej školy slovenského jazyka a kultúry Studia Academica Slovaca sa uskutočnil vďaka spolupráci so Slovenským národným korpusom,
+                                oddelením Jazykovedného ústavu Ľudovíta Štúra SAV v Bratislave.</small></p>
+                        </blockquote>
+                        <blockquote>
+                            <p><a href="http://slovniky.juls.savba.sk/?d=kssj4&d=sssj&d=sssj2&d=psp&d=scs&d=sss&d=peciar&d=bernolak&d=hssjV&d=obce&d=priezviska&d=un" target="_blank">On-line slovníky</a></p>
+                            <p><small>Jazykovedný ústav Ľudovíta Štúra SAV je pracoviskom, v ktorom sa v Slovenskej republike sústreďuje základný výskum spisovných aj nespisovných útvarov slovenského národného jazyka. Odkazom sa dostanete na aktuálnu ponuku dostupných slovníkov.</small></p>
                         </blockquote>
                     </div>
                 </div>
