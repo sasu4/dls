e61b4fe41b56b60d2ef12772ff00414955c263b1
Jakub <Jakub@10.10.106.12>
2016-02-03 10:34:05 +0100

    kontaktny formular

--- application/views/contact.php
+++ application/views/contact.php
@@ -2,8 +2,9 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-6">
-                <h1>Kontakt</h1>
-                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada</p>
+                <h1>Napíšte nám</h1>
+                <p>Ak sa zaujímate o možnosť štúdia slovenského jazyka v zahraničí, o aktivity Studia Academica Slovaka alebo si myslíte, že na stránke nejaká informácia chýba, budeme radi, keď nám napíšete.
+                </p>
             </div>
             <div class="col-sm-6">
                 <ul class="breadcrumb pull-right">
@@ -20,32 +21,56 @@
         <div class="col-sm-8">
             <h4>Napíšte nám</h4>
             <div class="status alert alert-success" style="display: none"></div>
-            <form id="main-contact-form" class="contact-form" name="contact-form" method="post" action="sendemail.php" role="form">
+            <?php echo form_open('contact/sent'); ?>
+            <!--<form id="main-contact-form" class="contact-form" name="contact-form" method="post" action="sendemail.php" role="form">-->
                 <div class="row">
                     <div class="col-sm-5">
                         <div class="form-group">
-                            <input type="text" class="form-control" required="required" placeholder="meno">
+                            <?php echo form_input('name','','required="required" class="form-control" placeholder="meno"'); ?>
+                            <!--<input type="text" class="form-control" required="required" placeholder="meno">-->
                         </div>
                         <div class="form-group">
-                            <input type="text" class="form-control" required="required" placeholder="priezvisko">
+                            <?php echo form_input('surname','','required="required" class="form-control" placeholder="priezvisko"'); ?>
+                            <!--<input type="text" class="form-control" required="required" placeholder="priezvisko">-->
                         </div>
                         <div class="form-group">
-                            <input type="text" class="form-control" required="required" placeholder="email">
+                            <?php echo form_input('email','','required="required" class="form-control" placeholder="email"'); ?>
+                            <!--<input type="text" class="form-control" required="required" placeholder="email">-->
                         </div>
                         <div class="form-group">
-                            <button type="submit" class="btn btn-primary btn-lg">Poslať správu</button>
+                            <?php echo form_submit('contact/sent', 'Poslať správu', 'class="btn btn-lg btn-primary"'); ?>
+                            <!--<button type="submit" class="btn btn-primary btn-lg">Poslať správu</button>-->
                         </div>
                     </div>
                     <div class="col-sm-7">
-                        <textarea name="message" id="message" required="required" class="form-control" rows="8" placeholder="správa"></textarea>
+                        <?php echo form_textarea('message','','required="required" class="form-control" rows="8" placeholder="správa"'); ?>
+                        <!--<textarea name="message" id="message" required="required" class="form-control" rows="8" placeholder="správa"></textarea>-->
                     </div>
                 </div>
-            </form>
+            <?php echo form_close(); ?>
+            <!--</form>-->
         </div><!--/.col-sm-8-->
         <div class="col-sm-4">
             <h4>Kde nás nájdete</h4>
             <!--<iframe width="100%" height="215" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com.au/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Dhaka,+Dhaka+Division,+Bangladesh&amp;aq=0&amp;oq=dhaka+ban&amp;sll=40.714353,-74.005973&amp;sspn=0.836898,1.815491&amp;ie=UTF8&amp;hq=&amp;hnear=Dhaka+Division,+Bangladesh&amp;t=m&amp;ll=24.542126,90.293884&amp;spn=0.124922,0.411301&amp;z=8&amp;output=embed"></iframe>-->
             <iframe  width="100%" height="400px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2662.432688876589!2d17.116372700000003!3d48.14046289999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476c8941d2b09c51%3A0x7037e8b88011c966!2sGondova+70%2F2%2C+811+02+Bratislava!5e0!3m2!1ssk!2ssk!4v1436442900150"></iframe>
+            <address>
+                <strong>Studia Academica Slovaca - centrum pre slovenčinu ako cudzí jazyk</strong><br>
+                Univerzita Komenského, Filozofická fakulta<br>
+                Gondova 2<br>
+                814 99 Bratislava, Slovensko<br/>
+                <abbr title="Phone"><i class="icon-phone"></i></abbr> +421 2 59 339 497<br/>
+                <abbr title="E-mail"><i class="icon-envelope"></i></abbr> sas@fphil.uniba.sk
+            </address>
+            <!--                <h4>Newsletter</h4>
+                            <form role="form">
+                <div class="input-group">
+                    <input type="text" class="form-control" autocomplete="off" placeholder="Enter your email">
+                    <span class="input-group-btn">
+                        <button class="btn btn-danger" type="button">Go!</button>
+                    </span>
+                </div>
+                            </form>-->
         </div><!--/.col-sm-4-->
     </div>
 </section><!--/#contact-page-->
